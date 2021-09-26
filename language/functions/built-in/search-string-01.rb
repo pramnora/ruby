814da005fc03f93b51e3d0d21c5e96fab4a5420a
Paul Ramnora <pramnora@yahoo.com>
2021-09-26 06:07:11 +0100

    Create search-string-01.rb
    Created file: [search-string-01.rb]/Demonstrates how to search text strings to find an included sub-string...

---
+++ language/functions/built-in/search-string-01.rb
@@ -0,0 +1,14 @@
+# Variable declaration...
+
+text = "Hello, world!"
+
+# Search text string variable to find a particular sub-string...
+
+puts text.include? "e"
+puts text.include? "ex"
+puts text.include? "el"
+
+# Output...
+# true
+# false
+# true
