0bd7af6603c5c34ff9e5f3bde4a3559d8cfe9e11
Paul Ramnora <pramnora@yahoo.com>
2021-09-26 14:14:47 +0100

    Update length-position-01.rb
    Refined/re-wrote explanation at the bottom...how Ruby counts characters.

--- language/functions/built-in/length-position-01.rb
+++ language/functions/built-in/length-position-01.rb
@@ -12,18 +12,11 @@ puts text[0]
 # H
 
 # Explanation...
-# 1234567890123
+# 1234567890123 (humans, count up from position one)
 # Hello, world!
+# 0123456789012 (Ruby, counts up from position zero)
 # ...thus, the text string: "Hello, world!" consists of 13 characters, altogether.
-# ----
-# NOTE: Ruby counts character position 1 as 0/2 as 1/-etc.
-#       therefore, puts text[0] will print out the 1st character, H
-#                  puts text[1] will print out the 2nd character, e
-#                  -etc.
-#       Or, in other words, as Ruby starts couning from zero...; 
-#       and, not one...as we would, normally, do...;
-#       therefore, Ruby sees the string of characters as looking like this:
-#       0123456789012
-#       Hello, world!
-#       1234567890123
-#       ...as opposed to counting up each character by using the bottom method.
+# but, becuase Ruby counts up from zero...; therefore,...
+# puts text[0]...prints out the 1st character: 'H' 
+# puts text[1]...prints out the 2nd character: 'e'
+# and, so on...
