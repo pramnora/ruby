5b65d7c2abf572ffde1f56fa542f6eb134e0eff9
Paul Ramnora <pramnora@yahoo.com>
2021-09-26 01:41:47 +0100

    Create hw01.rb
    Created file: [hw01.rb]/also, added comments...

---
+++ language/output/hw/hw01.rb
@@ -0,0 +1,8 @@
+# print text to output screen...
+print "Hello, world!"
+print "Hello, world!"
+
+# Output...
+# Hello, world!Hello, world!
+
+# NOTE: print, does NOT include a 'new line' at the end 
