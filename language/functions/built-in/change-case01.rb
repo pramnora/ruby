0fb96e588c69dc968def93cc2147cbbf20cfbb92
Paul Ramnora <pramnora@yahoo.com>
2021-09-26 02:17:01 +0100

    Create change-case01.rb
    Created file: [change-case01.rb]/Shows how to switch text casing to being either all upper case/or, all lower case characters...

---
+++ language/functions/built-in/change-case01.rb
@@ -0,0 +1,13 @@
+# variable declaration...
+msgText="Hello, world!"
+
+# print text message to output screen...
+puts msgText.upcase()   # show text as being all upper case letters
+puts msgText.downcase() # show text as being all lower case letters
+
+# NOTE: The pair of empty brackets: () at the end of both upcase()/downcase() are, actually, 'optional';
+#       so, writing it as: upcase/downcase...using no pair of brackets would also work.
+
+# Output...
+# HELLO, WORLD!
+# hello, world!
