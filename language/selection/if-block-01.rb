d7ceb17b34b4640ac386cbfe82c48b0b90af86aa
Paul Ramnora <pramnora@yahoo.com>
2021-09-26 06:44:54 +0100

    Create if-block-01.rb
    Created file: [if-block-01.rb]/Demonstrates the use of Ruby 'if block', including: if/elsif/else/end

---
+++ language/selection/if-block-01.rb
@@ -0,0 +1,18 @@
+# Variable declarations...
+
+x=1
+#x=2
+#x=3
+
+# if/elsif/else/end block...
+
+if x==1
+ puts("x=1")
+elsif x==2
+ puts("x=2")
+else 
+ puts("x NOT 1/x NOT 2")
+end
+
+# Output...
+# x=1
